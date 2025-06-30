7a677a4ef799d87eaf5d82fc119c8519fd689dda
鲍健昕 <1411643015@qq.com>
2025-06-30 23:13:55 +0800

    文件DEBUG级别日志，控制台INFO级别日志

--- main.py
+++ main.py
@@ -41,10 +41,17 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('【%(asctime)s】 - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 # 控制台处理器（显示 DEBUG 以上）
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
+# console_handler = logging.StreamHandler()
+# console_handler.setLevel(logging.DEBUG)
+# console_handler.setFormatter(formatter)
+# logger.addHandler(console_handler)
+
+
+log_file = 'eth_monitor.log'
+file_handler = logging.FileHandler(log_file, encoding='utf-8')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 # 手动设置代理
 # proxies = {
@@ -522,6 +529,7 @@ class TextHandler(logging.Handler):
 
 # 创建 TextHandler 并添加到 logger
 text_handler = TextHandler(log_text, max_lines=1000)
+text_handler.setLevel(logging.INFO)  # 设置为 INFO 级别
 text_handler.setFormatter(formatter)
 logger.addHandler(text_handler)
 
